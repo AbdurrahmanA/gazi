fc88796b609288a0bf12736815bc4d9f135ce3b6
AbdurrahmanA <abdurahman6673@gmail.com>
2022-11-10 11:48:32 +0300

    bu gereksiz bir dosya

--- tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
- float vki = (kilo / boy2);
-//  printf("%f \n",vki);
-//  if (vki >= 18,50 && vki < 25 ){
-//  printf("Askeri liseye basvurun kabul edildi");
-//  }
-//  else{
-//  printf("Askeri liseye basvurun kabul edilmedi");
-//  }
